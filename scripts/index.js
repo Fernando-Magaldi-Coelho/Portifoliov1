6e1f3ceba3608715fe7403fb889c2ae159bd7127
Fernando Magaldi Coelho <135265421+Fernando-Magaldi-Coelho@users.noreply.github.com>
2023-08-01 19:20:18 -0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -62,7 +62,7 @@ function scrollToPosition(to) {
 
 function scrollToIdOnClick(event) {
 	event.preventDefault();
-	const to = getScrollTopByHref(event.currentTarget)- 200;
+	const to = getScrollTopByHref(event.currentTarget)- 160;
 	scrollToPosition(to);
 }
 
